e12f7e2535f510e27753466b826000bdcb897525
vegaSeB <sebasquero2005@gmail.com>
2023-04-17 08:51:00 -0500

    Everything commented

--- src/main/java/co/edu/unbosque/controller/ServletUser.java
+++ src/main/java/co/edu/unbosque/controller/ServletUser.java
@@ -5,9 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @SuppressWarnings("serial")
 public class ServletUser extends HttpServlet {
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter salida = new PrintWriter(resp.getWriter());
